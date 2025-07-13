c8d78242ffeee78e679b2ccd8ca7f4efe81fe18b
Roman Khokhlachov <roman.khokhlachov@tuta.io>
2025-07-13 23:38:47 +0300

    Split shared structures into valid on-disk and in-memory versions

--- common/shared_structs.h
+++ common/shared_structs.h
@@ -1,70 +1,87 @@
+/*
+ * This header defines structures shared between kernel module (portfs) and
+ * userspace utility.
+ * Structures prefixed with 'portfs_disk_' represent on-disk layout.
+ * Structures without 'disk' are in-memory and may differ.
+ */
+
 #ifndef SHARED_STRUCTS_H
 #define SHARED_STRUCTS_H
 
 #ifdef __KERNEL__
 #include <linux/types.h>
+typedef __be16 portfs_be16;
+typedef __be32 portfs_be32;
+typedef __be64 portfs_be64;
 #else
 #include <cstdint>
+typedef uint16_t portfs_be16;
+typedef uint32_t portfs_be32;
+typedef uint64_t portfs_be64;
 #endif // __KERNEL__
 
-//const uint16_t MAX_EXTENTS = 8;
-#define MAX_EXTENTS 8
+#define DIRECT_EXTENTS 4
 
-#ifdef __KERNEL__
+// #ifdef __KERNEL__
 
 struct portfs_disk_superblock {
-    __be32 magic_number;
-    __be32 block_size;
-    __be32 total_blocks;
-    __be32 filetable_start;    // Offset in blocks
-    __be32 filetable_size;     // Size in blocks
-    __be32 block_bitmap_start; // Offset in blocks
-    __be32 block_bitmap_size;  // Size in blocks
-    __be32 data_start;         // Offset in blocks
-    __be32 max_file_count;
-    __be32 checksum;
+    portfs_be32 magic_number;
+    portfs_be32 block_size;
+    portfs_be32 total_blocks;
+    portfs_be32 filetable_start;    // Offset in blocks
+    portfs_be32 filetable_size;     // Size in blocks
+    portfs_be32 block_bitmap_start; // Offset in blocks
+    portfs_be32 block_bitmap_size;  // Size in blocks
+    portfs_be32 data_start;         // Offset in blocks
+    portfs_be32 max_file_count;
+    portfs_be32 checksum;
 
-    __be64 last_mount_time;
-    __be64 last_write_time;
-    __be32 flags;
+    portfs_be64 last_mount_time;
+    portfs_be64 last_write_time;
+    portfs_be32 flags;
 } __attribute__((packed));
 
 struct disk_extent
 {
-    __be32 start_block;
-    __be32 length;
+    portfs_be32 start_block;
+    portfs_be32 length;
 }__attribute__((packed));
 
 struct disk_filetable_entry
 {
     char name[64];
-    __be64 ino;
-    __be64 sizeInBytes;
-    __be16 extentCount;
-    struct disk_extent extents[MAX_EXTENTS];
+    portfs_be64 ino;
+    portfs_be64 size_in_bytes;
+    portfs_be16 extent_count;
+    portfs_be32 extents_block;
+    struct disk_extent direct_extents[DIRECT_EXTENTS];
 }__attribute__((packed));
 
-#endif // __KERNEL__
+// #endif // __KERNEL__
 
 #ifdef __cplusplus
 extern "C"
 {
-#endif
+#endif // __cplusplus
 
 struct extent
 {
     uint32_t start_block;
     uint32_t length;
-} __attribute__((packed));
+};
 
 struct filetable_entry
 {
     char name[64];
     uint64_t ino;
-    uint64_t sizeInBytes;
-    uint16_t extentCount;
-    struct extent extents[MAX_EXTENTS];
-} __attribute__((packed));
+    uint64_t size_in_bytes;
+    uint16_t extent_count;
+    uint32_t extents_block;
+    struct extent direct_extents[DIRECT_EXTENTS];
+#ifdef __KERNEL__
+    struct extent *indirect_extents;
+#endif // __KERNEL__
+};
 
 struct portfs_superblock
 {
@@ -89,10 +106,10 @@ struct portfs_superblock
 
     struct super_block *super;
 #endif
-} __attribute__((packed));
+};
 
 #ifdef __cplusplus
 } // extern "C"
-#endif
+#endif // __cplusplus
 
 #endif // SHARED_STRUCTS_H
